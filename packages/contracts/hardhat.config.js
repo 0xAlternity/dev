7f0da685f02de503b9c1736a78b8271bc80fb9ac
Smol Fork <101562437+smol-fork@users.noreply.github.com>
2023-07-20 14:28:44 +0400

    add flat task

--- packages/contracts/hardhat.config.js
+++ packages/contracts/hardhat.config.js
@@ -29,6 +29,20 @@ const alchemyUrlRinkeby = () => {
   return `https://eth-rinkeby.alchemyapi.io/v2/${getSecret("alchemyAPIKeyRinkeby")}`;
 };
 
+task("flat", "Flattens and prints contracts and their dependencies (Resolves licenses)")
+  .addOptionalVariadicPositionalParam("files", "The files to flatten", undefined, types.inputFile)
+  .setAction(async ({ files }, hre) => {
+    let flattened = await hre.run("flatten:get-flattened-sources", { files });
+    
+    // Remove every line started with "// SPDX-License-Identifier:"
+    flattened = flattened.replace(/SPDX-License-Identifier:/gm, "License-Identifier:");
+    flattened = `// SPDX-License-Identifier: MIXED\n\n${flattened}`;
+
+    // Remove every line started with "pragma experimental ABIEncoderV2;" except the first one
+    flattened = flattened.replace(/pragma experimental ABIEncoderV2;\n/gm, ((i) => (m) => (!i++ ? m : ""))(0));
+    console.log(flattened);
+  });
+
 module.exports = {
   paths: {
     // contracts: "./contracts",
