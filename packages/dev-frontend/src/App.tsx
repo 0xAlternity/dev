64778704c0e2fddde6c31838cf8c493422412f8e
Smol Fork <101562437+smol-fork@users.noreply.github.com>
2023-07-29 12:14:40 +0400

    update frontend visual assets

--- packages/dev-frontend/src/App.tsx
+++ packages/dev-frontend/src/App.tsx
@@ -42,34 +42,6 @@ const EthersWeb3ReactProvider: React.FC = ({ children }) => {
   );
 };
 
-const UnsupportedMainnetFallback: React.FC = () => (
-  <Flex
-    sx={{
-      flexDirection: "column",
-      alignItems: "center",
-      justifyContent: "center",
-      height: "100vh",
-      textAlign: "center"
-    }}
-  >
-    <Heading sx={{ mb: 3 }}>
-      <Icon name="exclamation-triangle" /> This app is for testing purposes only.
-    </Heading>
-
-    <Paragraph sx={{ mb: 3 }}>
-      Please change your network to Ropsten, Rinkeby, Kovan or Görli.
-    </Paragraph>
-
-    <Paragraph>
-      If you'd like to use the Liquity Protocol on mainnet, please pick a frontend{" "}
-      <Link href="https://www.liquity.org/frontend">
-        here <Icon name="external-link-alt" size="xs" />
-      </Link>
-      .
-    </Paragraph>
-  </Flex>
-);
-
 const App = () => {
   const loader = (
     <Flex sx={{ alignItems: "center", justifyContent: "center", height: "100vh" }}>
@@ -100,11 +72,7 @@ const App = () => {
     <EthersWeb3ReactProvider>
       <ThemeProvider theme={theme}>
         <WalletConnector loader={loader}>
-          <LiquityProvider
-            loader={loader}
-            unsupportedNetworkFallback={unsupportedNetworkFallback}
-            unsupportedMainnetFallback={<UnsupportedMainnetFallback />}
-          >
+          <LiquityProvider loader={loader} unsupportedNetworkFallback={unsupportedNetworkFallback}>
             <TransactionProvider>
               <LiquityFrontend loader={loader} />
             </TransactionProvider>
